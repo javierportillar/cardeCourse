b21ae014ce6772a79a3f8830c908f3fc9fca3875
“javierportillar” <javierandres.008@hotmail.com>
2024-06-10 10:30:20 -0500

    Added getAuthorById sending req-params.id as param for getAuthorByID service

--- src/controllers/authors.controller.js
+++ src/controllers/authors.controller.js
@@ -5,6 +5,13 @@ class AuthorsController {
     const authors = await authorsService.getAllAuthors();
     return res.json(authors).status(200);
   }
+
+  async getAuthorById(req, res) {
+    const author = await authorsService.getAuthorById(req.params.id);
+    return author
+      ? res.json(author).status(200)
+      : res.status(404).json({ message: "not found" });
+  }
 }
 
 const authorsController = new AuthorsController();
