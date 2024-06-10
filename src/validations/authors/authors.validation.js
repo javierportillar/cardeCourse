18c724f4ab720850a7f8f66f52bf3ed5dfc19637
“javierportillar” <javierandres.008@hotmail.com>
2024-06-10 14:28:36 -0500

    Added validateAuthorsSchema to check inputs creating new author

--- src/validations/authors/authors.validation.js
+++ src/validations/authors/authors.validation.js
@@ -2,4 +2,18 @@ const { param, body, checkSchema } = require("express-validator");
 
 const validateParamsId = param("id", "Must be a number").isNumeric().exists();
 
-module.exports = { validateParamsId };
+const validateAuthorsSchema = checkSchema(
+  {
+    name: { isString: true, exists: true, isLength: { options: { max: 20 } } },
+    age: { isNumeric: true, optional: true },
+    gender: {
+      isString: true,
+      isLength: { options: { max: 2 } },
+    },
+    categories: { isArray: { options: { min: 1 } } },
+    "categories.*": { isNumeric: true },
+  },
+  ["body"]
+);
+
+module.exports = { validateParamsId, validateAuthorsSchema };
