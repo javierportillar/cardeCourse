04df1ebcd1bced8ca58869a3174f688aaca3c916
“javierportillar” <javierandres.008@hotmail.com>
2024-06-10 06:49:43 -0500

    Added authorsRouter inyo index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { authorsRouter } = require("./routes/authors.router");
 const { booksRouter } = require("./routes/books.router");
 const { Server } = require("./server/server");
 const { config } = require("dotenv");
@@ -11,6 +12,7 @@ config({
 const server = new Server();
 
 server.addRouter("/books", booksRouter);
+server.addRouter("/authors", authorsRouter);
 
 server.createServer(process.env.PORT, () => {
   console.log("Listening on Port", process.env.PORT);
