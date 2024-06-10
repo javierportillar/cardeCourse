edc3eabc3718ba1dc044a50e6af15acbbc2434ba
“javierportillar” <javierandres.008@hotmail.com>
2024-06-10 14:30:24 -0500

    added createAuthor and createAuthorsCategories but got problems using more than 1 category_id into author

--- src/services/authors.service.js
+++ src/services/authors.service.js
@@ -14,6 +14,53 @@ class AuthorsService {
     return author;
   }
 
+  async createAuthor(body) {
+    const { name, age, gender, categories } = body;
+    const ageValue = age !== undefined ? `'${age}'` : "NULL";
+    const genderValue = gender ? `'${gender}'` : "NULL";
+    const categorieValue = categories ? `'${categories}'` : "NULL";
+    const author = await database.create(
+      `INSERT INTO authors (name, age, gender, category_id) VALUES('${name}',${ageValue},${genderValue},${categorieValue})`
+    );
+    // await this.createAuthorsCategories(categorieValue, author.id);
+
+    return author;
+  }
+
+
+  async createAuthorsCategories(categories, authorId) {
+    if (categories && categories.every((categoryId) => categoryId !== null)) {
+      const arrayAuthorsCategories = categories.map((categoryId) => {
+        return `( ${authorId},${categoryId})`;
+      });
+      const SQLvalues = arrayAuthorsCategories.join(",");
+      const result = await database.createMany(
+        `INSERT INTO authors_categories (author_id, category_id) VALUES ${SQLvalues}`
+      );
+      return result;
+    } else {
+      throw new Error("Invalid categories array");
+    }
+  }
+
+  // async createAuthorsCategories(categories, authorId) {
+  //   if (categories && categories.every((categoryId) => categoryId !== null)) {
+  //     await this.createAuthorsCategories(categories, author.id);
+  //   } else {
+  //     throw new Error("Invalid categories array");
+  //   }
+
+  //   const arrayAuthorsCategories = categories.map((categoryId) => {
+  //     return `( ${authorId},${categoryId})`;
+  //   });
+  //   const SQLvalues = arrayAuthorsCategories.join(",");
+  //   console.log(SQLvalues);
+  //   const result = await database.createMany(
+  //     `INSERT INTO authors_categories (author_id, category_id) VALUES ${SQLvalues}`
+  //   );
+  //   return result;
+  // }
+
   async checkAuthors(ids) {
     const SQLids = ids.join(",");
     const result = await database.getRows(
