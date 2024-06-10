36a8d3d9ac734e2179f6c2d5a107a2d789277c34
“javierportillar” <javierandres.008@hotmail.com>
2024-06-10 10:32:21 -0500

    Added get by path /id to get 1 author

--- src/routes/authors.router.js
+++ src/routes/authors.router.js
@@ -1,8 +1,22 @@
 const { Router } = require("express");
-const {authorsController} = require("../controllers/authors.controller")
+const { authorsController } = require("../controllers/authors.controller");
+const {
+  validateErrosMiddleWare,
+} = require("../middlewares/validateErros.middleware");
+
+const {
+  validateParamsId,
+} = require("../validations/authors/authors.validation");
+
 const authorsRouter = Router();
 
 //Obtener Authores
-authorsRouter.get("/", authorsController.getAllAuthors)
+authorsRouter.get("/", authorsController.getAllAuthors);
 
-module.exports = {authorsRouter};
+authorsRouter.get("/:id", [
+  validateParamsId,
+  validateErrosMiddleWare,
+  authorsController.getAuthorById,
+]);
+
+module.exports = { authorsRouter };
