085df8b5fbbac5f919c63a0c34642d07f458b78a
“javierportillar” <javierandres.008@hotmail.com>
2024-06-10 10:17:10 -0500

    Added getAuthorById sending id as param into SQL query being sent to database.getRow

--- src/services/authors.service.js
+++ src/services/authors.service.js
@@ -5,6 +5,15 @@ class AuthorsService {
     const authors = await database.getRows("SELECT * FROM AUTHORS");
     return authors;
   }
+
+  async getAuthorById(id) {
+    console.log(id);
+    const author = await database.getRow(
+      `SELECT * FROM AUTHORS WHERE id=${id} limit 1`
+    );
+    return author;
+  }
+
   async checkAuthors(ids) {
     const SQLids = ids.join(",");
     const result = await database.getRows(
