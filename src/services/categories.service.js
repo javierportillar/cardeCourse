89ba70280af263a8cc25519e240efa2e09dcd13f
“javierportillar” <javierandres.008@hotmail.com>
2024-06-11 13:13:33 -0500

    Created new service of get All categories and get by id with SQL Request

--- src/services/categories.service.js
+++ src/services/categories.service.js
@@ -1,11 +1,23 @@
-const {database} = require('../database/database');
+const { database } = require("../database/database");
 
 class CategoriesService {
+  async getAllCategories() {
+    return await database.getRows("SELECT * FROM categories");
+  }
 
-  async checkCategories(ids){
-    const SQLids = ids.join(',')
-    const result = await database.getRows(`SELECT id FROM categories WHERE id IN(${SQLids})`);
-    
+  async getCategoryById(id) {
+    const categorie = await database.getRow(
+      `SELECT * FROM categories WHERE id=${id} limit 1`
+    );
+    return categorie;
+  }
+
+  async checkCategories(ids) {
+    const SQLids = ids.join(",");
+    const result = await database.getRows(
+      `SELECT id FROM categories WHERE id IN(${SQLids})`
+    );
+
     return result.length === ids.length; // ESTO ES DE CRACKS, DE PROS.
 
     // if (result.length === ids.lenght){
@@ -17,4 +29,4 @@ class CategoriesService {
 
 const categoriesService = new CategoriesService();
 
-module.exports = {categoriesService};
+module.exports = { categoriesService };
