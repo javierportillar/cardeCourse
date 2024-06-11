394454962d6f0bdc75c47d1308ba6efc54c025d2
“javierportillar” <javierandres.008@hotmail.com>
2024-06-11 14:35:56 -0500

    Added validateCatSchema for body input

--- src/validations/categories/categories.validation.js
+++ src/validations/categories/categories.validation.js
@@ -1,5 +1,8 @@
 const { param, body, checkSchema } = require("express-validator");
 
 const validateParamsId = param("id", "Must be a number").isNumeric().exists();
+const validateCategoriesSchema = checkSchema({
+  name: { isString: true, exists: true, isLength: { options: { max: 20 } }},
+},['body'])
 
-module.exports = { validateParamsId };
+module.exports = { validateParamsId, validateCategoriesSchema };
