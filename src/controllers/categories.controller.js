916a5caa687f7ee032b0fe47a966a52cff509a07
“javierportillar” <javierandres.008@hotmail.com>
2024-06-11 14:33:06 -0500

    Added creaeteNewCategory async fcn and got trouble setting same name

--- src/controllers/categories.controller.js
+++ src/controllers/categories.controller.js
@@ -13,6 +13,14 @@ class CategoriesController {
       ? res.json(category).status(200)
       : res.status(404).json({ message: "not found" });
   }
+
+  async createNewCategory(req,res){
+    // const checkCategValidate = await categoriesService.checkCategories(req.body.name);
+    // if (checkCategValidate) return res.status(409).json({message: 'Ya existe la categoría'}); Pendiente por categoría repitente
+    
+    const category = await categoriesService.createCategory(req.body);
+    return res.json(category).status(201);
+  }
 }
 
 const categoriesController = new CategoriesController();
